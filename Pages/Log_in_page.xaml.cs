26224e3be7e468ef4d32dcf9eb0efb8319d26363
Diego Arriaga <dielarri@ut.utm.edu>
2021-11-03 21:50:57 -0500

    Login pages

---
+++ Pages/Log_in_page.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Main_Menu
+{
+    /// <summary>
+    /// Interaction logic for Log_in_page.xaml
+    /// </summary>
+    public partial class Log_in_page : Page
+    {
+        public Log_in_page()
+        {
+            InitializeComponent();
+        }
+    }
+}
