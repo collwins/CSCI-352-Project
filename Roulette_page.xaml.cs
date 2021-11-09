7ae358185081512794b4347c8d0757ca8283c288
Diego Arriaga <dielarri@ut.utm.edu>
2021-11-08 22:41:18 -0600

    Add project files.

---
+++ Roulette_page.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Main_Menu
+{
+    /// <summary>
+    /// Interaction logic for Roulette_page.xaml
+    /// </summary>
+    public partial class Roulette_page : Page
+    {
+        public Roulette_page()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Num_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+    }
+}
